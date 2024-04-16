53045ff4127275e35659763586ef60579c5a0308
ddddincn <edudddd@163.com>
2024-04-16 20:14:32 +0800

    update buffer

--- buffer/buffer.h
+++ buffer/buffer.h
@@ -12,12 +12,34 @@
 class Buffer
 {
 public:
-    Buffer();
-    ~Buffer();
+    Buffer(int bufferSize);
+    ~Buffer() = default;
 
-    size_t readableBytes() const;
-    size_t writeableBytes() const;
-    size_t prependableBytes() const;
+    size_t readableBytes() const;    // 返回可写字节数
+    size_t writableBytes() const;   // 返回可读字节数
+    size_t prependableBytes() const; // 返回已读字节数
+
+    const char *peek() const; // 返回读取位置的指针
+
+    void retrieve(size_t len);
+    void retrieveUntil(const char *end);
+    void retrieveAll();
+    std::string retrieveAllToStr();
+
+    void hasWritten(size_t len);
+
+    char *beginWrite();
+    void ensureWriteable(size_t len);
+
+    void append(const char *data, size_t len);
+
+    ssize_t writeFd(int fd, int *saveErrno);
+    ssize_t readFd(int fd, int *saveErrno);
+
+private:
+    char *beginPtr_();
+    const char *beginPtr_() const;
+    void makeSpace_(size_t len);
 
 private:
     std::vector<char> buff_;
