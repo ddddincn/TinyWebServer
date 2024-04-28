67913a41bd701eaf984d343c7b04b1704e2b5eb8
ddddincn <edudddd@163.com>
2024-04-28 22:54:10 +0800

    fix bug

--- log/blockqueue.h
+++ log/blockqueue.h
@@ -1,15 +1,14 @@
 #ifndef BLOCKQUEUE_H
 #define BLOCKQUEUE_H
 
-#include <mutex>
-#include <queue>
 #include <cassert>
 #include <condition_variable>
+#include <mutex>
+#include <queue>
 
 template <typename T>
-class BlockQueue
-{
-public:
+class BlockQueue {
+   public:
     explicit BlockQueue(size_t maxCapacity = 1024);
     ~BlockQueue();
 
@@ -19,15 +18,15 @@ public:
     void flush();
     void close();
 
-    void push(const T &item); // 队尾添加元素
-    bool pop(T &item);        // 队首删除元素
-    T front();                // 返回队首元素
-    T back();                 // 返回队尾元素
+    void push(const T &item);  // 队尾添加元素
+    bool pop(T &item);         // 队首删除元素
+    T front();                 // 返回队首元素
+    T back();                  // 返回队尾元素
 
     size_t size();
     size_t capacity();
 
-private:
+   private:
     std::queue<T> que_;
     std::mutex mtx_;
     size_t capacity_;
@@ -37,115 +36,101 @@ private:
 };
 
 template <typename T>
-BlockQueue<T>::BlockQueue(size_t maxCapacity) : capacity_(maxCapacity)
-{
+BlockQueue<T>::BlockQueue(size_t maxCapacity) : capacity_(maxCapacity) {
     assert(maxCapacity > 0);
     isClose_ = false;
 }
 
 template <typename T>
-BlockQueue<T>::~BlockQueue()
-{
+BlockQueue<T>::~BlockQueue() {
     std::lock_guard<std::mutex> locker(mtx_);
-    std::queue<T>().swap(que_); // 清空队列操作
+    std::queue<T>().swap(que_);  // 清空队列操作
     isClose_ = true;
     condConsumer_.notify_all();
     condProducer_.notify_all();
 }
 
 template <typename T>
-void BlockQueue<T>::clear()
-{
+void BlockQueue<T>::clear() {
     std::lock_guard<std::mutex> locker(mtx_);
-    std::queue<T>().swap(que_); // 清空队列操作
+    std::queue<T>().swap(que_);  // 清空队列操作
 }
 
 template <typename T>
-bool BlockQueue<T>::empty()
-{
+bool BlockQueue<T>::empty() {
     std::lock_guard<std::mutex> locker(mtx_);
     return que_.empty();
 }
 
 template <typename T>
-bool BlockQueue<T>::full()
-{
+bool BlockQueue<T>::full() {
     std::lock_guard<std::mutex> locker(mtx_);
     return que_.size() > capacity_;
 }
 
 template <typename T>
-void BlockQueue<T>::flush()
-{
+void BlockQueue<T>::flush() {
     condConsumer_.notify_one();
 }
 
 template <typename T>
-void BlockQueue<T>::close()
-{
+void BlockQueue<T>::close() {
     std::lock_guard<std::mutex> locker(mtx_);
-    std::queue<T>().swap(que_); // 清空队列操作
+    std::queue<T>().swap(que_);  // 清空队列操作
     isClose_ = true;
     condConsumer_.notify_all();
     condProducer_.notify_all();
 }
 
 template <typename T>
-void BlockQueue<T>::push(const T &item) // push()由生产者调用
+void BlockQueue<T>::push(const T &item)  // push()由生产者调用
 {
     std::unique_lock<std::mutex> locker(mtx_);
-    while (que_.size() >= capacity_)
-    {
-        condProducer_.wait(locker); // 如果队列满了，生产者会阻塞
+    while (que_.size() >= capacity_) {
+        condProducer_.wait(locker);  // 如果队列满了，生产者会阻塞
     }
     que_.push(item);
     condConsumer_.notify_one();
 }
 
 template <typename T>
-bool BlockQueue<T>::pop(T &item) // pop()由消费者调用
+bool BlockQueue<T>::pop(T &item)  // pop()由消费者调用
 {
     std::unique_lock<std::mutex> locker(mtx_);
-    while (que_.empty())
-    {
-        if (isClose_)
-        {
+    while (que_.empty()) {
+        if (isClose_) {
             return false;
         }
-        condConsumer_.wait(locker); // 如果队列为空，消费者会等待生产者唤醒(当生产者push()成功时会唤醒)
+        condConsumer_.wait(locker);  // 如果队列为空，消费者会等待生产者唤醒(当生产者push()成功时会唤醒)
     }
     item = que_.front();
     que_.pop();
-    condProducer_.notify_one(); //
+    condProducer_.notify_one();  //
     return true;
 }
 
 template <typename T>
-T BlockQueue<T>::front()
-{
+T BlockQueue<T>::front() {
     std::lock_guard<std::mutex> locker(mtx_);
     return que_.front();
 }
 
 template <typename T>
-T BlockQueue<T>::back()
-{
+T BlockQueue<T>::back() {
     std::lock_guard<std::mutex> locker(mtx_);
     return que_.back();
 }
 
 template <typename T>
-size_t BlockQueue<T>::size()
-{
+size_t BlockQueue<T>::size() {
     std::lock_guard<std::mutex> locker(mtx_);
     return que_.size();
 }
 
 template <typename T>
-size_t BlockQueue<T>::capacity()
-{
+size_t BlockQueue<T>::capacity() {
     std::lock_guard<std::mutex> locker(mtx_);
     return capacity_;
 }
 
-#endif // BLOCKQUEUE_H
+#endif  // BLOCKQUEUE_H
--- log/log.h
+++ log/log.h
@@ -1,21 +1,21 @@
 #ifndef LOG_H
 #define LOG_H
 
-#include <mutex>
-#include <thread>
-#include <cassert>
-#include <cstdarg>
 #include <sys/stat.h>
 #include <sys/time.h>
 
-#include "blockqueue.h"
-#include "../buffer/buffer.h"
+#include <cassert>
+#include <cstdarg>
+#include <mutex>
+#include <thread>
 
-class Log
-{
-public:
+#include "../buffer/buffer.h"
+#include "blockqueue.h"
+
+class Log {
+   public:
     void init(int level, const char *path = "./log", const char *suffix = ".log", int maxCapacity = 1024);
-    static Log *instance(); // 单例模式
+    static Log *instance();  // 单例模式
     void flushLogThread();
     void write(int leve, const char *format, ...);
     void flush();
@@ -24,50 +24,60 @@ public:
     void setLevel(int level);
     bool isClose();
 
-private:
+   private:
     Log();
     ~Log();
     void appendLogLevelTitle(int level);
     void asyncWrite();
 
-private:
+   private:
     static const int MAX_PATH_LEN = 256;
     static const int MAX_NAME_LEN = 256;
     static const int MAX_LINES = 50000;
 
-    const char *path_;   // 路径
-    const char *suffix_; // 后缀
+    const char *path_;    // 路径
+    const char *suffix_;  // 后缀
 
     int maxLines_;
     int lineCount_;
-    int today_; // 记录当天时间
+    int today_;  // 记录当天时间
 
-    Buffer buff_; // 缓存区
+    Buffer buff_;  // 缓存区
     int level_;
 
     bool isClose_;
     bool isAsync_;
 
     FILE *fp_;
-    std::unique_ptr<BlockQueue<std::string>> que_; // 阻塞队列，异步写入使用
-    std::unique_ptr<std::thread> writeThread_;     // 异步写入线程
+    std::unique_ptr<BlockQueue<std::string>> que_;  // 阻塞队列，异步写入使用
+    std::unique_ptr<std::thread> writeThread_;      // 异步写入线程
     std::mutex mtx_;
 };
 // 这里用do()while(0)来写宏是为了防止编译器歧义
-#define LOG_BASE(level, format, ...)                   \
-    do                                                 \
-    {                                                  \
-        Log *log = Log::instance();                    \
-        if (!log->isClose() && log->getLevel() <= level) \
-        {                                              \
-            log->write(level, format, ##__VA_ARGS__);  \
-            log->flush();                              \
-        }                                              \
+#define LOG_BASE(level, format, ...)                       \
+    do {                                                   \
+        Log *log = Log::instance();                        \
+        if (!log->isClose() && log->getLevel() <= level) { \
+            log->write(level, format, ##__VA_ARGS__);      \
+            log->flush();                                  \
+        }                                                  \
     } while (0);
 
-#define LOG_DEBUG(format, ...) do {LOG_BASE(0, format, ##__VA_ARGS__)} while(0);
-#define LOG_INFO(format, ...) do {LOG_BASE(1, format, ##__VA_ARGS__)} while(0);
-#define LOG_WARN(format, ...) do {LOG_BASE(2, format, ##__VA_ARGS__)} while(0);
-#define LOG_ERROR(format, ...) do {LOG_BASE(3, format, ##__VA_ARGS__)} while(0);
+#define LOG_DEBUG(format, ...)             \
+    do {                                   \
+        LOG_BASE(0, format, ##__VA_ARGS__) \
+    } while (0);
+#define LOG_INFO(format, ...)              \
+    do {                                   \
+        LOG_BASE(1, format, ##__VA_ARGS__) \
+    } while (0);
+#define LOG_WARN(format, ...)              \
+    do {                                   \
+        LOG_BASE(2, format, ##__VA_ARGS__) \
+    } while (0);
+#define LOG_ERROR(format, ...)             \
+    do {                                   \
+        LOG_BASE(3, format, ##__VA_ARGS__) \
+    } while (0);
 
-#endif // LOG_H
+#endif  // LOG_H
